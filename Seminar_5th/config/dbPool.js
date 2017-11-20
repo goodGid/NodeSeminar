aa6efdf98e37ebb67e3569b7fe31e64361ee8fea
Kiyong Shin <gy8971@gmail.com>
2017-11-21 02:43:33 +0900

    Update dbPool.js

--- Seminar_5th/config/dbPool.js
+++ Seminar_5th/config/dbPool.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const dbConfig = {
-	host : 'rds-goodgid.c4hdhslyc6js.ap-northeast-2.rds.amazonaws.com',
-	port : '3306',
-	user : 'goodgid',
-	password : 'rldyd0413!',
-	database : '5th_homework',
+	host : '',
+	port : '',
+	user : '',
+	password : '!',
+	database : '',
 	connectionLimit : 10
 };
 
